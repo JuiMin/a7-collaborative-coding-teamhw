f0f9bddd3a0bb0b377ad67ccc3e3afb191785fc5
edwardlxb <lux23@uw.edu>
2016-02-21 16:33:28 -0800

    chartV1. close#3

---
+++ scripts/Chart1.R
@@ -0,0 +1,14 @@
+##install.packages("plotrix")
+library(jsonlite)
+library(plotly)
+library(dplyr)
+
+setwd("/Users/Edward/Documents/a7-collaborative-coding-teamhw/data")
+survey<- read.csv(file = 'intro_survey_data.csv')
+
+map<- function(data){
+  histogram<- hist(data, breaks=12, col="grey65", 
+                   main= "Histogram from Survey Data", 
+                   border="blue")
+  return(histogram)
+}
