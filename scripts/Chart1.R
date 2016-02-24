5871080de200aa28e9f28109a38a6029f8b81cb7
edwardlxb <lux23@uw.edu>
2016-02-23 20:51:29 -0800

    ChartV3. close#3

--- scripts/Chart1.R
+++ scripts/Chart1.R
@@ -3,12 +3,14 @@ library(jsonlite)
 library(plotly)
 library(dplyr)
 
-##setwd("/Users/Edward/Documents/a7-collaborative-coding-teamhw/data")
+setwd("/Users/Edward/Documents/a7-collaborative-coding-teamhw/data")
 survey<- read.csv(file = 'intro_survey_data.csv')
 
 map<- function(data){
-  histogram<- hist(data, breaks=12, col="grey65", 
+  histogram<- hist(data<- survey$How.many.countries.have.you.visited.in.your.life., breaks=12, col="grey65", 
                    main= "Histogram from Survey Data", 
                    border="blue")
   return(histogram)
 }
+
+
